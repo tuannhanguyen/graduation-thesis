9d9d01415d631644aa759ee52ddaf7d941585644
tuannhan <tuannhan6@gmail.com>
2022-09-05 23:49:56 +0700

    init

---
+++ TuannhanShopProject/TuannhanShopWebParent/TuannhanShopBackEnd/src/main/java/com/eshop/admin/TuannhanShopBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.eshop.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuannhanShopBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuannhanShopBackEndApplication.class, args);
+	}
+
+}
