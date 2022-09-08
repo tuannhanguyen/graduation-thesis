3a0de66415f6ee04ea7537dc863cf1c5d3c31ac2
tuannhan <nhan.nvtn@tctav.com>
2022-09-08 15:57:12 +0700

    Sorting for users list

--- TuannhanShopProject/TuannhanShopWebParent/TuannhanShopBackEnd/src/main/java/com/eshop/admin/user/UserController.java
+++ TuannhanShopProject/TuannhanShopWebParent/TuannhanShopBackEnd/src/main/java/com/eshop/admin/user/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,27 +26,33 @@ public class UserController {
 
 	@GetMapping("/users")
 	public String listFirstPage(Model model) {
-		return listByPage(1, model);
+		return listByPage(1, model, "firstName", "asc");
 	}
-	
+
 	@GetMapping("/users/page/{pageNum}")
-	public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model) {
-		Page<User> page = userService.listByPage(pageNum);
+	public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model,
+			@Param("sortField") String sortField, @Param("sortDir") String sortDir) {
+		Page<User> page = userService.listByPage(pageNum, sortField, sortDir);
 		List<User> listUsers = page.getContent();
-		
+
 		long startCount = (pageNum - 1) * UserService.USERS_PER_PAGE + 1;
 		long endCount = startCount + UserService.USERS_PER_PAGE - 1;
 		if (endCount > page.getTotalElements()) {
 			endCount = page.getTotalElements();
 		}
-		
+
+		String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+
 		model.addAttribute("currentPage", pageNum);
 		model.addAttribute("totalPages", page.getTotalPages());
 		model.addAttribute("startCount", startCount);
 		model.addAttribute("endCount", endCount);
 		model.addAttribute("totalItems", page.getTotalElements());
 		model.addAttribute("listUsers", listUsers);
-		
+		model.addAttribute("sortField", sortField);
+		model.addAttribute("sortDir", sortDir);
+		model.addAttribute("reverseSortDir", reverseSortDir);
+
 		return "users";
 	}
 
@@ -70,14 +77,14 @@ public class UserController {
 			String fileName = org.springframework.util.StringUtils.cleanPath(multipartFile.getOriginalFilename());
 			user.setPhotos(fileName);
 			User savedUser = userService.save(user);
-			
+
 			String uploadDir = "users-photos/" + savedUser.getId();
-			
+
 			FileUploadUtil.cleanDir(uploadDir);
 			FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
 		} else {
 			if (user.getPhotos().isEmpty()) user.setPhotos(null);
-			userService.save(user);			
+			userService.save(user);
 		}
 
 		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
