5b5725888729dadfd28d320e627fc42d109c3c5c
tuannhan <nhan.nvtn@tctav.com>
2022-09-09 15:14:33 +0700

    user login page

--- TuannhanShopProject/TuannhanShopWebParent/TuannhanShopBackEnd/src/main/java/com/eshop/admin/security/WebSecurityConfig.java
+++ TuannhanShopProject/TuannhanShopWebParent/TuannhanShopBackEnd/src/main/java/com/eshop/admin/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.eshop.admin.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,10 +17,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public PasswordEncoder PasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests().anyRequest().permitAll();
+		http.authorizeHttpRequests()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.formLogin()
+				.loginPage("/login")
+				.usernameParameter("email")
+				.permitAll();
 	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**");
+	}
+
+
 
 }
