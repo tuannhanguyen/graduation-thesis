1620ab9b78b029da1f2a95f007caa871e1e009ba
tuannhan <nhan.nvtn@tctav.com>
2022-09-09 14:05:30 +0700

    filter users

--- TuannhanShopProject/TuannhanShopWebParent/TuannhanShopBackEnd/src/main/java/com/eshop/admin/user/UserRepository.java
+++ TuannhanShopProject/TuannhanShopWebParent/TuannhanShopBackEnd/src/main/java/com/eshop/admin/user/UserRepository.java
@@ -1,8 +1,9 @@
 package com.eshop.admin.user;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.eshop.common.entity.User;
@@ -12,6 +13,9 @@ public interface UserRepository extends PagingAndSortingRepository<User, Integer
 	public User getUserByEmail(String email);
 
 	public Long countById(Integer id);
+
+	@Query("SELECT u from User u WHERE CONCAT(u.id, ' ', u.email, ' ', u.firstName, ' ', u.lastName) LIKE %?1%")
+	public Page<User> findAll(String keyword, Pageable pageable);
 
 	@Query("UPDATE User u SET u.enabled = ?2 WHERE u.id = ?1")
 	@Modifying
