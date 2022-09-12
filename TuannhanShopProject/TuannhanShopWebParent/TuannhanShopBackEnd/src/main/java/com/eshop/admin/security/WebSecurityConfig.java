45bbc9b127f51ea384e9960f4a97aa361d5f631f
tuannhan <tuannhan6@gmail.com>
2022-09-12 19:48:57 +0700

    authorization basics

--- TuannhanShopProject/TuannhanShopWebParent/TuannhanShopBackEnd/src/main/java/com/eshop/admin/security/WebSecurityConfig.java
+++ TuannhanShopProject/TuannhanShopWebParent/TuannhanShopBackEnd/src/main/java/com/eshop/admin/security/WebSecurityConfig.java
@@ -43,6 +43,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests()
+			.antMatchers("/users/**").hasAuthority("Admin")
 			.anyRequest()
 			.authenticated()
 			.and()
