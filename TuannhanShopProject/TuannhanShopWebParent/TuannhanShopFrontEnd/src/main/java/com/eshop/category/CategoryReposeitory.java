ebddaa1ba79b28ddf25258ba38f396046f5612d5
tuannhan <nhan.nvtn@tctav.com>
2022-09-26 14:42:28 +0700

    Listing product in category

--- TuannhanShopProject/TuannhanShopWebParent/TuannhanShopFrontEnd/src/main/java/com/eshop/category/CategoryReposeitory.java
+++ TuannhanShopProject/TuannhanShopWebParent/TuannhanShopFrontEnd/src/main/java/com/eshop/category/CategoryReposeitory.java
@@ -12,4 +12,7 @@ public interface CategoryReposeitory extends CrudRepository<Category, Integer> {
 	@Query("SELECT c FROM Category c WHERE c.enabled = true ORDER BY c.name ASC")
 	public List<Category> findAllEnabled();
 
+	@Query("SELECT c FROM Category c WHERE c.enabled = true AND c.alias = ?1")
+	public Category findByAliasEnabled(String alias);
+
 }
--- TuannhanShopProject/TuannhanShopWebParent/TuannhanShopFrontEnd/src/main/java/com/eshop/category/CategoryService.java
+++ TuannhanShopProject/TuannhanShopWebParent/TuannhanShopFrontEnd/src/main/java/com/eshop/category/CategoryService.java
@@ -27,4 +27,8 @@ public class CategoryService {
 
 		return listNoChildrenCategories;
 	}
+
+	public Category getCategory(String alias) {
+		return categoryReposeitory.findByAliasEnabled(alias);
+	}
 }
