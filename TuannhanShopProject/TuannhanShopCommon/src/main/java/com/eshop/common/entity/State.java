21c693857d68f4640a3b3b199ff8c49f03072d0d
tuannhan <nhan.nvtn@tctav.com>
2022-09-28 10:35:29 +0700

    Country and State entity

---
+++ TuannhanShopProject/TuannhanShopCommon/src/main/java/com/eshop/common/entity/State.java
@@ -0,0 +1,51 @@
+package com.eshop.common.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "states")
+public class State {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(nullable = false, length = 45)
+	private String name;
+
+	@ManyToOne
+	@JoinColumn(name = "country_id")
+	private Country country;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Country getCountry() {
+		return country;
+	}
+
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+
+}
---
+++ TuannhanShopProject/TuannhanShopWebParent/TuannhanShopBackEnd/src/main/java/com/eshop/admin/setting/state/StateRepository.java
@@ -0,0 +1,14 @@
+package com.eshop.admin.setting.state;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.eshop.common.entity.Country;
+import com.eshop.common.entity.State;
+
+public interface StateRepository extends CrudRepository<State, Integer>{
+
+	public List<State> findByCountryOrderByNameAsc(Country country);
+
+}
