70d51d7dc1f7c8466ce61591163a6fa31c3e15fd
tuannhan <tuannhan6@gmail.com>
2022-10-12 00:04:04 +0700

    Address Book module

--- TuannhanShopProject/TuannhanShopCommon/src/main/java/com/eshop/common/entity/Address.java
+++ TuannhanShopProject/TuannhanShopCommon/src/main/java/com/eshop/common/entity/Address.java
@@ -1,7 +1,5 @@
 package com.eshop.common.entity;
 
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,54 +10,47 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "customers")
-public class Customer {
+@Table(name = "addresses")
+public class Address {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-
-	@Column(nullable = false, unique = true, length = 45)
-	private String email;
-
-	@Column(nullable = false, length = 64)
-	private String password;
-
+	
 	@Column(name = "first_name", nullable = false, length = 45)
 	private String firstName;
-
+	
 	@Column(name = "last_name", nullable = false, length = 45)
 	private String lastName;
-
-	@Column(name = "phone_number", nullable = false, length = 15)
+	
+	@Column(name = "phone_number", nullable = false, length = 125)
 	private String phoneNumber;
-
-	@Column(nullable = false, length = 64)
+	
+	@Column(name = "address_line_1", nullable = false, length = 64)
 	private String addressLine1;
-
-	@Column(name = "address_line_2", nullable = false, length = 64)
+	
+	@Column(name = "address_line_2", length = 64)
 	private String addressLine2;
-
+	
 	@Column(nullable = false, length = 45)
 	private String city;
-
+	
 	@Column(nullable = false, length = 45)
 	private String state;
-
-	@Column(name = "postal_code", nullable = false, length = 10)
+	
+	@Column(name = "postal_code", nullable = false, length = 64)
 	private String postalCode;
-
-	@Column(name = "verification_code", length = 64)
-	private String verificationCode;
-
-	private boolean enabled;
-
-	@Column(name = "created_time")
-	private Date createdTime;
-
+	
 	@ManyToOne
 	@JoinColumn(name = "country_id")
 	private Country country;
+	
+	@ManyToOne
+	@JoinColumn(name = "customer_id")
+	private Customer customer;
+	
+	@Column(name = "default_address")
+	private boolean defaultForShipping;
 
 	public Integer getId() {
 		return id;
@@ -67,22 +58,6 @@ public class Customer {
 
 	public void setId(Integer id) {
 		this.id = id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String emai) {
-		this.email = emai;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
 	}
 
 	public String getFirstName() {
@@ -149,30 +124,6 @@ public class Customer {
 		this.postalCode = postalCode;
 	}
 
-	public String getVerificationCode() {
-		return verificationCode;
-	}
-
-	public void setVerificationCode(String verificationCode) {
-		this.verificationCode = verificationCode;
-	}
-
-	public boolean isEnabled() {
-		return enabled;
-	}
-
-	public void setEnabled(boolean enabled) {
-		this.enabled = enabled;
-	}
-
-	public Date getCreatedTime() {
-		return createdTime;
-	}
-
-	public void setCreatedTime(Date createdTime) {
-		this.createdTime = createdTime;
-	}
-
 	public Country getCountry() {
 		return country;
 	}
@@ -180,12 +131,23 @@ public class Customer {
 	public void setCountry(Country country) {
 		this.country = country;
 	}
-	
-	@javax.persistence.Transient
-	public String getFullName() {
-		return this.firstName + " " + this.lastName;
+
+	public Customer getCustomer() {
+		return customer;
 	}
-	
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public boolean isDefaultForShipping() {
+		return defaultForShipping;
+	}
+
+	public void setDefaultForShipping(boolean defaultForShipping) {
+		this.defaultForShipping = defaultForShipping;
+	}
+
 	@Override
 	public String toString() {
 		String address = firstName;
@@ -207,5 +169,7 @@ public class Customer {
 		
 		return address;
 	}
-
+	
+	
+	
 }
